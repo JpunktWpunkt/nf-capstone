77702faded6e90fe91ae514d4a68e9d666e3911c
JpunktWpunkt <92968754+JpunktWpunkt@users.noreply.github.com>
2022-02-04 23:14:50 +0100

    Feature/add page layout (#14)
    * chore: add material ui
    
    * feat: add mui buttons
    
    * feat:header and footer
    
    * changes from pullrequest
    
    * add layout to userpage

--- src/pages/user/[username]/index.jsx
+++ src/pages/user/[username]/index.jsx
@@ -1,9 +1,14 @@
 import { useRouter } from "next/router";
 import React from "react";
+import Layout from "../../../organisms/layout";
 
 const Page = () => {
 	const { query } = useRouter();
-	return <div>Hallo {query.username}</div>;
+	return (
+		<Layout>
+			<div>Hallo {query.username}</div>
+		</Layout>
+	);
 };
 
 export default Page;
