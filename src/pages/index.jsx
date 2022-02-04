46ff2dd7af04682d83c1161a655dccb3499e6e38
JpunktWpunkt <92968754+JpunktWpunkt@users.noreply.github.com>
2022-02-04 23:21:14 +0100

    feature: add login page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,10 @@
+import { TextField } from "@mui/material";
 import Button from "@mui/material/Button";
 import Head from "next/head";
 import Link from "next/link";
 import React from "react";
 import Login from "../organisms/login";
+
 
 const Page = () => {
 	return (
@@ -11,8 +13,14 @@ const Page = () => {
 				<title key="title">FamilyHub</title>
 				<meta key="description" name="description" content="This is my project" />
 			</Head>
-			<Link passHref href="/home">
-				<Button variant="outlined">Login</Button>
+			<br />
+			<TextField id="outlined-basic" label="Username" variant="outlined" />
+			<br />
+			<br />
+			<TextField id="outlined-basic" label="Password" variant="outlined" type="password" />
+			<br />
+			<br />
+			<Link passHref href="/home"><Button variant="outlined">Login</Button>
 			</Link>
 		</Login>
 	);
