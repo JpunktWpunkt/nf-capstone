7567a9d89ba3d4fc245da2489000a735299021cf
JpunktWpunkt <92968754+JpunktWpunkt@users.noreply.github.com>
2022-02-08 15:33:26 +0100

    Db communication (#25)
    * chore:
    
    * 24# feat: First steps in DB with next.js and mongoDB, install Mongoose, added db.json, added model, update gitignore
    
    * Delete .env

--- src/molecules/noteItem/index.jsx
+++ src/molecules/noteItem/index.jsx
@@ -1,18 +1,25 @@
-import Toolbar from "@mui/material/Toolbar";
-import * as React from 'react';
-import TextareaAutosize from '@mui/material/TextareaAutosize';
-import Card from '@mui/material/Card';
+import DialogTitle from "@mui/material/DialogTitle";
+import CloseIcon from "@mui/icons-material/Close";
+import IconButton from "@mui/material/IconButton";
+import * as React from "react";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
+import Card from "@mui/material/Card";
 
-
-// eslint-disable-next-line react/function-component-definition
-export default function NoteItem({ note }) {
+const NoteItem = ({ note }) => {
 	return (
 		<div>
-			<Card variant="outlined" sx={{maxWidth: 345 }}>
-				<TextareaAutosize aria-label="empty textarea" placeholder="Put your Notes here" defaultValue={note.text} style={{ width: 400, height: 400 }}/>
+			<Card variant="outlined" sx={{ maxWidth: 345 }}>
+				<IconButton>
+					<TextareaAutosize
+						aria-label="empty textarea"
+						placeholder="Put your Notes here"
+						defaultValue={note.text}
+						style={{ width: 400, height: 400 }}
+					/>
+				</IconButton>
 			</Card>
 		</div>
 	);
 };
 
-
+export default NoteItem;
--- src/organisms/header/index.jsx
+++ src/organisms/header/index.jsx
@@ -1,29 +1,28 @@
 import Button from "@mui/material/Button";
 import Link from "next/link";
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
-
-
- const Header = () => {
+const Header = () => {
 	return (
 		<Box sx={{ flexGrow: 1 }}>
 			<AppBar position="static">
 				<Toolbar>
-
 					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
 						FamilyHub
 					</Typography>
 					<Link passHref href="/">
-						<Button color="inherit" variant="outlined">Abmelden</Button>
+						<Button color="inherit" variant="outlined">
+							Logout
+						</Button>
 					</Link>
 				</Toolbar>
 			</AppBar>
 		</Box>
 	);
-}
+};
 
 export default Header;
