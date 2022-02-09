390c0bd123ce7472a83fe0ff9588b27e246ac848
JpunktWpunkt <92968754+JpunktWpunkt@users.noreply.github.com>
2022-02-09 14:33:57 +0100

    feat: add Schema

--- model/todo.model.js
+++ model/todo.model.js
@@ -5,6 +5,17 @@ const todoSchema = new mongoose.Schema({
 	isChecked: Boolean,
 });
 
+export default mongoose.models.Todo || mongoose.model("Todo", todoSchema);
+
+/*
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+	name: String,
+	isChecked: Boolean,
+});
+
 const Todo = mongoose.model("Todo", todoSchema);
 
 export default Todo;
+*/
--- src/pages/api/database/index.js
+++ src/pages/api/database/index.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 import process from "node:process";
 import Todo from "../../../../model/todo.model";
+import dbConnect from "../../../../database/index";
 
-const connect = async uri => {
+/*const connect = async uri => {
 	if (!uri) {
 		throw new Error("No uri was provided");
 	}
@@ -15,11 +16,34 @@ const connect = async uri => {
 	}
 };
 
-void connect(process.env.MONGODB_URI);
+void connect(process.env.MONGODB_URI);*/
 
 const handler = async (request, response) => {
-	const mongoResponse = await Todo.find();
-	response.status(200).json(mongoResponse);
+	const { method } = request;
+	await dbConnect;
+
+	switch (method) {
+		case "GET":
+			try {
+				const mongoResponse = await Todo.find();
+				response.status(200).json(mongoResponse);
+			} catch (err) {
+				console.log(err);
+			}
+			break;
+
+		case "POST":
+			try {
+				const mongoResponse = await Todo.create(request.body);
+				response.status(200).json(mongoResponse);
+			} catch (err) {
+				console.log(err);
+			}
+			break;
+
+		default:
+			response.status(404).send("not found");
+	}
 };
 
 export default handler;
