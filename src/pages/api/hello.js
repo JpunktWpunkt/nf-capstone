8de3e05624bf7501accf7ea32302a64d3c96cc9d
JpunktWpunkt <wittig.jessica@googlemail.com>
2022-02-10 09:03:03 +0100

    edit hello.js

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -23,9 +23,9 @@ export default async function handler(request, response) {
 	const myTodo2 = new Todo({ name: "Bar" });
 	const myTodo3 = new Todo({ name: "FooBar" });
 
-	/*	await myTodo1.save();
+	await myTodo1.save();
 	await myTodo2.save();
-	await myTodo3.save();*/
+	await myTodo3.save();
 	const mongoResponse = await Todo.find();
 	response.status(200).json(mongoResponse);
 }
