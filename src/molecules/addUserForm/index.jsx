7b1387c88659dbf9a4b0b9b9c0ebf2e933d9f1fe
JpunktWpunkt <92968754+JpunktWpunkt@users.noreply.github.com>
2022-02-21 13:42:01 +0100

    Feature/add user (#36)
    * create a Add User component and added to main page, edit API Route users
    
    * create a Add User component and added to main page, edit API Route users2
    
    * Added User completed
    
    * fixed api user file
    
    * added a hard refresh....

---
+++ src/molecules/addUserForm/index.jsx
@@ -0,0 +1,70 @@
+import axios from "axios";
+import React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+const btnAdd = {
+	borderRadius: "6px 6px 0 0",
+	display: "flex",
+	position: "fixed",
+	width: "100vw",
+	zIndex: 1,
+	left: 0,
+	fontSize: "1.5rem",
+	bottom: "4.5rem",
+	background: "#5B764A",
+	padding: "0.5rem 1rem",
+};
+
+const AddUserForm = () => {
+	const [open, setOpen] = React.useState(false);
+	const [username, setUsername] = React.useState("");
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const handleAddUser = async () => {
+		await axios.post("/api/users", { username: username }); //1,Parameter aus der DB 2.Parameter aus useState
+		setOpen(false);
+		window.location.reload();
+	};
+
+	return (
+		<div>
+			<Button variant="contained" sx={{ ...btnAdd }} onClick={handleClickOpen}>
+				+ Add New User
+			</Button>
+			<Dialog open={open} onClose={handleClose}>
+				<DialogTitle>New User</DialogTitle>
+				<DialogContent>
+					<DialogContentText>Add a new pinboard user</DialogContentText>
+					<TextField
+						autoFocus
+						margin="dense"
+						id="name"
+						label="Name"
+						type="name"
+						fullWidth
+						variant="standard"
+						onChange={e => setUsername(e.target.value)}
+					/>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose}>Cancel</Button>
+					<Button onClick={handleAddUser}>Enter User</Button>
+				</DialogActions>
+			</Dialog>
+		</div>
+	);
+};
+export default AddUserForm;
--- src/pages/api/users/index.js
+++ src/pages/api/users/index.js
@@ -26,6 +26,20 @@ const handler = async (request, response) => {
 
 		default:
 			response.status(404).send("not found");
+
+		/*		case "PUT":
+			try {
+				console.log("try body request", request.body);
+				const mongoResponse = await User.findByIdAndUpdate(request.body.noteId, {
+					content: request.body.content,
+				});
+				console.log("mongoDB response", mongoResponse);
+				response.status(200).json(mongoResponse);
+			} catch (err) {
+				console.log(err);
+				response.status(403).send("ups, something went wrong");
+			}
+			break;*/
 	}
 };
 
